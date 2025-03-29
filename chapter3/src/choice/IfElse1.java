e3001e380dfa25431401a5127708209392a704a3
kosangjin7 <kosangjin7@gmail.com>
2025-03-29 20:24:19 +0900

    first commit

---
+++ chapter3/src/choice/IfElse1.java
@@ -0,0 +1,29 @@
+package choice;
+
+public class IfElse1 {
+
+	public static void main(String[] args) {
+		//if문
+		//나이가 15세 이상이면 "관람가" 출력
+		int age = 16;
+		
+		if(age >= 15) {
+			System.out.println("관람가");				
+		}
+		System.out.println("나이는" + age + "세 입니다.");
+		
+		//if ~ else문
+		//나이가 15세 이상이면 "관람가"이고, 아니면 "관람불가" 출력
+		if(age >= 15) {
+			System.out.println("관람가");
+		}else {
+			System.out.println("관람불가");
+		}
+		System.out.println("나이는" + age + "세 입니다.");
+		
+				
+		}
+			
+	}
+
+
---
+++ day03/src/day03/VarTest.java
@@ -0,0 +1,22 @@
+package day03;
+
+public class VarTest {
+
+	public static void main(String[] args) {
+
+		int x=10;
+		float f=3.14f;
+		double d=3.14;
+		char c='a';
+		String str="hello";
+		
+		System.out.println(x);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(str);
+		
+		
+	}
+
+}
